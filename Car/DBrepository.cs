f1b071b369c7cdacc5af7795ff23bce150206934
belvl <belvl@tomskcable.ru>
2022-06-08 16:53:23 +0700

    Создал класс для ДБ репозитория, но не пойму как им пользоваться

---
+++ Car/DBrepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    class DbRepository
+    {
+        private ICar _car;
+
+        public DbRepository(ICar car)
+        {
+            _car = car;
+        }
+
+        public void AddDB ()
+        {
+        
+        }
+    }
+}
