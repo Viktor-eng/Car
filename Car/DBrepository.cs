25ae9e7c7e14794875172abf53dfc24d313fb861
belvl <belvl@tomskcable.ru>
2022-06-09 14:08:49 +0700

    Очищен класс DbRepository

--- Car/DBrepository.cs
+++ Car/DBrepository.cs
@@ -1,23 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Car
+﻿namespace Car
 {
     class DbRepository
     {
-        private ICar _car;
 
-        public DbRepository(ICar car)
-        {
-            _car = car;
-        }
-
-        public void AddDB ()
-        {
-        
-        }
     }
 }
